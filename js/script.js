b58c6f5ed77698dd3d73a9a9fa0d57531c21929b
dmitryshtern <dimashtern@mail.ru>
2022-04-09 17:31:26 +0300

    English_training_Obj_wordDatabase

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 "use strict";
 
 const nav = document.querySelector('.nav'),
-      title = document.querySelector('.intro_title');
+      title = document.querySelector('.intro_title'),
+      textNav = document.querySelector('.text_content');
+
+
+const wordDatabase = {
+    questionWords: ['Where', 'Why', 'When', 'How', 'Who', 'What' ],
+    verbs: [],
+    pronouns: []
+};
+
+
+// Navigator item
+
+function questionButton() {
+    const randomIndex = Math.floor(Math.random() * wordDatabase.questionWords.length); 
+    const randomElement = wordDatabase.questionWords[randomIndex];
+    return randomElement;
+
+}
 
 nav.addEventListener('click', () => { 
-    title.textContent = '';        
+    textNav.textContent = `${questionButton()}`;  
+          
 });
-
 
 
 
