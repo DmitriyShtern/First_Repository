7d2371d27f6eeae3d5182af344fdbed3f0cae1c5
dmitryshtern <dimashtern@mail.ru>
2022-04-09 15:20:08 +0300

    English_training_Search

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const nav = document.querySelector('.nav'),
+      title = document.querySelector('.intro_title');
+
+nav.addEventListener('click', () => { 
+    title.textContent = '';        
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
