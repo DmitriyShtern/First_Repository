90042889ebf4ee94c479733d23bb2a13abb6e848
dmitryshtern <dimashtern@mail.ru>
2022-04-10 11:16:23 +0300

    English_training_nav.addEventListener

--- js/script.js
+++ js/script.js
@@ -2,31 +2,43 @@
 
 const nav = document.querySelector('.nav'),
       title = document.querySelector('.intro_title'),
-      textNav = document.querySelector('.text_content');
+      textNav = document.querySelector('.text_content'),
+      navItem = document.querySelectorAll('.nav_link');
 
 
-const wordDatabase = {
-    questionWords: ['Where', 'Why', 'When', 'How', 'Who', 'What' ],
-    verbs: [],
-    pronouns: []
-};
+const wordDatabase = [
+    ['Where', 'Why', 'When', 'How', 'Who', 'What'],
+    ['dance', 'do', 'cook', 'sing', 'play', 'read', 'etc'],
+    []
+];
 
 
 // Navigator item
 
-function questionButton() {
-    const randomIndex = Math.floor(Math.random() * wordDatabase.questionWords.length); 
-    const randomElement = wordDatabase.questionWords[randomIndex];
+function randomWord(i) {
+    const randomIndex = Math.floor(Math.random() * i.length); 
+    const randomElement = i[randomIndex];                       // Возвращает рандомное слово из вопросов
     return randomElement;
 
 }
 
-nav.addEventListener('click', () => { 
-    textNav.textContent = `${questionButton()}`;  
-          
-});
+
+const keys = Object.keys( wordDatabase );    // Получаю массив из названия ключей объекта wordDatabase
 
 
+nav.addEventListener('click', (event) => { 
+    const target = event.target;
+    if(target && target.classList.contains('nav_link') ) {
+        navItem.forEach((item, i) => {
+           
+            if (target == item) {
+                textNav.textContent = randomWord(wordDatabase[i]);
+            }
+    });
+    
+}});
+    
+   
 
 
 
