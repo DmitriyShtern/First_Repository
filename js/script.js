61ce5c28eaa54469fe3e132ce97216d2100474a6
dmitryshtern <dimashtern@mail.ru>
2022-04-12 18:48:19 +0300

    English_training_test_Database_JSON_ENG

--- js/script.js
+++ js/script.js
@@ -1,75 +1,73 @@
 "use strict";
 
 const nav = document.querySelector('.nav'),
-      title = document.querySelector('.intro_title'),
-      textNav = document.querySelector('.text_content'),
-      navItem = document.querySelectorAll('.nav_link'),
-      wrapperInput = document.querySelector('.input');
-      
-      
-      
+    title = document.querySelector('.intro_title'),
+    textNav = document.querySelector('.text_content'),
+    navItem = document.querySelectorAll('.nav_link'),
+    wrapperInput = document.querySelector('.input'),
+    autoFocus = document.querySelector('#fname');
 
 
-const wordEngDatabase = [
-    ['Where', 'Why', 'When', 'How', 'Who', 'What'],
-    ['Dance', 'Do', 'Cook', 'Sing', 'Play', 'Read'],
-    ['I', 'You', 'He', 'she', 'We', 'They'],
-    ['action', 'activity', 'age', 'air', 'animal', 'area', 'authority', 'bank', 'body', 'book']
-];
 
 
-const wordRuDatabase = [
-    ['Где', 'Почему', 'Когда', 'Как', 'Кто', 'Что'],
-    ['Танцевать', 'Делать', 'Готовить', 'Петь', 'Играть', 'Читать'],
-    ['я', 'ты', 'он', 'она', 'мы', 'они'],
-    ['действие', 'Мероприятия', 'возраст', 'воздух', 'животное', 'область', 'орган власти', 'банк', 'тело', 'книга']
-];
+// const wordEngDatabase = [
+//     ['Where', 'Why', 'When', 'How', 'Who', 'What'],
+//     ['Dance', 'Do', 'Cook', 'Sing', 'Play', 'Read'],
+//     ['I', 'You', 'He', 'she', 'We', 'They'],
+//     ['action', 'activity', 'age', 'air', 'animal', 'area', 'authority', 'bank', 'body', 'book'],
+//     []
+// ];
+
+
+// const wordRuDatabase = [
+//     ['Где', 'Почему', 'Когда', 'Как', 'Кто', 'Что'],
+//     ['Танцевать', 'Делать', 'Готовить', 'Петь', 'Играть', 'Читать'],
+//     ['я', 'ты', 'он', 'она', 'мы', 'они'],
+//     ['действие', 'Мероприятия', 'возраст', 'воздух', 'животное', 'область', 'орган власти', 'банк', 'тело', 'книга'],
+//     []
+// ];
 
 // Navigator item
 
 function randomWord(i) {
-    const randomIndex = Math.floor(Math.random() * i.length); 
-    const randomElement = i[randomIndex];                       // Возвращает рандомное слово из вопросов
+    const randomIndex = Math.floor(Math.random() * i.length);
+    const randomElement = i[randomIndex]; // Возвращает рандомное слово из вопросов
     return randomElement;
 
 }
 
+function getDataBase(i) {
+    const request = new XMLHttpRequest();
 
-function showAutoFocus() {
-    const autoFocus = document.querySelector('#fname');
-    autoFocus.autofocus = true;
-    console.dir(autoFocus);
-    return autoFocus.autofocus;
+    request.open('GET', 'db.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            textNav.textContent = randomWord(data.database[i]);
+        }
+    });
 }
 
-
-nav.addEventListener('click', (event) => { 
+nav.addEventListener('click', (event) => {
     const target = event.target;
 
-    if(target && target.classList.contains('nav_link') ) {
+    if (target && target.classList.contains('nav_link')) {
         navItem.forEach((item, i) => {
-           
+
             if (target == item) {
-                textNav.textContent = randomWord(wordEngDatabase[i]);
+                getDataBase(i);
 
-                // showAutoFocus();
-
+                autoFocus.focus();
             }
-    });
-    
-}});
-    
 
 
-     // Сделать что бы при нажатии на кнопку глагол появлялся автофокус на поле ввода   			  
-        			
-        			
-        			 
-        			 
-        			    
-        			
-        			
-        			
+        });
+
+    }
+});
 
 
 
@@ -85,24 +83,4 @@ nav.addEventListener('click', (event) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Сделать что бы при нажатии на кнопку глагол появлялся автофокус на поле ввода   			  
